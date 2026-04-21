a499fa86c8e2570804f94261077b5e4b7d50c9b7
cocopo @ tiny <coco.poley@tiny.cloud>
2026-04-21 16:30:38 +0200

    Update TinyMCEEditor.vue

--- src/TinyMCEEditor.vue
+++ src/TinyMCEEditor.vue
@@ -1,33 +1,27 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
 import Editor from '@tinymce/tinymce-vue'
 
-const apiKey = 'no-api-key'
-const editorInit = ref({})
-
-onMounted(() => {
-  editorInit.value = {
-    plugins:
-      'tinymceai advlist anchor autolink charmap code fullscreen help image insertdatetime link lists media preview searchreplace table visualblocks wordcount',
-    toolbar:
-      'tinymceai-chat tinymceai-quickactions tinymceai-review | undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-    autoresize_overflow_padding: 50,
-    content_style: 'body { line-height: 1.5; }',
-    content_css: 'document',
-    height: '100%',
-    content_id: 'document-123',
-    tinymceai_token_provider: async () => {
-      await fetch('https://demo.api.tiny.cloud/1/unique-tinymce-key/auth/random', { method: "POST", credentials: "include" });
-      return { token: await fetch('https://demo.api.tiny.cloud/1/unique-tinymce-key/jwt/tinymceai', { credentials: "include" }).then(r => r.text()) };
-    },
-  }
-})
+const tokenProvider = async () => {
+  await fetch('https://demo.api.tiny.cloud/1/unique-tinymce-key/auth/random', { method: 'POST', credentials: 'include' });
+  return { token: await fetch('https://demo.api.tiny.cloud/1/unique-tinymce-key/jwt/tinymceai', { credentials: 'include' }).then(r => r.text()) };
+}
 </script>
 
 <template>
-  <editor
+  <Editor
     id="document-content"
-    :api-key="apiKey"
-    :init="editorInit"
+    apiKey="no-api-key"
+    :init="{
+      plugins: 'tinymceai advlist anchor autolink charmap code fullscreen help image insertdatetime link lists media preview searchreplace table visualblocks wordcount',
+      toolbar: 'tinymceai-chat tinymceai-quickactions tinymceai-review | undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+      autoresize_overflow_padding: 50,
+      content_style: 'body { line-height: 1.5; }',
+      content_css: 'document',
+      height: '100%',
+      content_id: 'document-123',
+      tinymceai_token_provider: tokenProvider,
+    }"
   />
 </template>
+
+<style></style>
